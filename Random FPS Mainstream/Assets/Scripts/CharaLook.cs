04b31e88e8f6d6a7a343bccefe53883999834759
Jules Vannier <julesvannier44@gmail.com>
2021-06-21 20:34:57 +0200

    Fondamentaux Characontroller
    >Mouvements
    >Camera move
    >Air drag

---
+++ Random FPS Mainstream/Assets/Scripts/CharaLook.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaLook : MonoBehaviour
+{
+    [SerializeField] private float m_sensX = 100.0f;
+    [SerializeField] private float m_sensY = 100.0f;
+
+    private Camera m_playerCamera;
+
+    private float m_mouseX;
+    private float m_mouseY;
+
+    private float m_multiplier = 0.01f;
+
+    private float m_xRotation;
+    private float m_yRotation;
+
+    private void Start()
+    {
+        m_playerCamera = GetComponentInChildren<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+
+        m_playerCamera.transform.localRotation = Quaternion.Euler(m_xRotation, 0, 0);
+        transform.rotation = Quaternion.Euler(0, m_yRotation, 0);
+    }
+
+    private void PlayerInput()
+    {
+        m_mouseX = Input.GetAxisRaw("Mouse X");
+        m_mouseY = Input.GetAxisRaw("Mouse Y");
+
+        m_yRotation += m_mouseX * m_sensX * m_multiplier;
+        m_xRotation -= m_mouseY * m_sensY * m_multiplier;
+
+        m_xRotation = Mathf.Clamp(m_xRotation, -90.0f, 90.0f);
+    }
+}
---
+++ Random FPS Mainstream/Assets/Scripts/CharaMovement.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using UnityEngine;
+
+public class CharaMovement : MonoBehaviour
+{
+    private float m_playerHeight = 2.0f;
+
+    [Header("Bindings")] [SerializeField] private KeyCode m_jumpKey = KeyCode.Space;
+    
+    [Header("Movement")] 
+    public float m_moveSpeed = 6.0f; 
+    private float m_movMultiplier = 10.0f;
+    [SerializeField] private float m_airMultiplier = 0.4f;
+    
+    [Header("Drag")]
+    private float m_groundedDrag = 6.0f;
+    private float m_airDrag = 2.0f;
+
+    [Header("Jumping")] 
+    [SerializeField] private float m_jumpForce = 5.0f; 
+
+    private float m_horizontalMov;
+    private float m_verticalMov;
+    private bool m_isGrounded;
+    private Vector3 m_moveDirection;
+    private Rigidbody m_playerRb;
+
+    private void Start()
+    {
+        m_playerRb = GetComponent<Rigidbody>();
+        m_playerRb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        m_isGrounded = Physics.Raycast(transform.position, Vector3.down, m_playerHeight / 2 + 0.1f);
+        
+        PlayerInputs();
+        ControlDrag();
+
+        if (Input.GetKeyDown(m_jumpKey) && m_isGrounded)
+        {
+            PlayerJump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerInputs()
+    {
+        m_horizontalMov = Input.GetAxisRaw("Horizontal");
+        m_verticalMov = Input.GetAxisRaw("Vertical");
+
+        m_moveDirection = transform.forward * m_verticalMov + transform.right * m_horizontalMov;
+    }
+
+    private void PlayerMove()
+    {
+        if (m_isGrounded)
+        {
+            m_playerRb.AddForce(m_moveDirection.normalized * m_moveSpeed * m_movMultiplier, ForceMode.Acceleration);
+        }
+        else if (!m_isGrounded)
+        {
+            m_playerRb.AddForce(m_moveDirection.normalized * m_moveSpeed * m_movMultiplier * m_airMultiplier, ForceMode.Acceleration);
+
+        }
+    }
+
+    private void PlayerJump()
+    {
+        m_playerRb.AddForce(transform.up * m_jumpForce, ForceMode.Impulse);
+    }
+
+    private void ControlDrag()
+    {
+        if (m_isGrounded)
+        {
+            m_playerRb.drag = m_groundedDrag;
+        }
+        else
+        {
+            m_playerRb.drag = m_airDrag;
+        }
+    }
+}
