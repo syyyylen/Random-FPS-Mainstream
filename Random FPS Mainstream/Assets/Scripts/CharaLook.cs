bee1f71e5a476cde6ffd1c0fee92477991031190
Jules Vannier <julesvannier44@gmail.com>
2021-06-21 23:54:35 +0200

    Camera fixes

--- Random FPS Mainstream/Assets/Scripts/CharaLook.cs
+++ Random FPS Mainstream/Assets/Scripts/CharaLook.cs
@@ -8,7 +8,8 @@ public class CharaLook : MonoBehaviour
     [SerializeField] private float m_sensX = 100.0f;
     [SerializeField] private float m_sensY = 100.0f;
 
-    private Camera m_playerCamera;
+    [SerializeField] private Transform m_playerCamera;
+    [SerializeField] private Transform m_orientation;
 
     private float m_mouseX;
     private float m_mouseY;
@@ -20,21 +21,11 @@ public class CharaLook : MonoBehaviour
 
     private void Start()
     {
-        m_playerCamera = GetComponentInChildren<Camera>();
-
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     private void Update()
-    {
-        PlayerInput();
-
-        m_playerCamera.transform.localRotation = Quaternion.Euler(m_xRotation, 0, 0);
-        transform.rotation = Quaternion.Euler(0, m_yRotation, 0);
-    }
-
-    private void PlayerInput()
     {
         m_mouseX = Input.GetAxisRaw("Mouse X");
         m_mouseY = Input.GetAxisRaw("Mouse Y");
@@ -43,5 +34,8 @@ public class CharaLook : MonoBehaviour
         m_xRotation -= m_mouseY * m_sensY * m_multiplier;
 
         m_xRotation = Mathf.Clamp(m_xRotation, -90.0f, 90.0f);
+
+        m_playerCamera.transform.localRotation = Quaternion.Euler(m_xRotation, m_yRotation, 0);
+        m_orientation.transform.rotation = Quaternion.Euler(0, m_yRotation, 0);
     }
 }
--- Random FPS Mainstream/Assets/Scripts/CharaMovement.cs
+++ Random FPS Mainstream/Assets/Scripts/CharaMovement.cs
@@ -5,24 +5,31 @@ public class CharaMovement : MonoBehaviour
 {
     private float m_playerHeight = 2.0f;
 
+    [SerializeField] private Transform m_orientation;
+
     [Header("Bindings")] [SerializeField] private KeyCode m_jumpKey = KeyCode.Space;
     
     [Header("Movement")] 
     public float m_moveSpeed = 6.0f; 
     private float m_movMultiplier = 10.0f;
     [SerializeField] private float m_airMultiplier = 0.4f;
+    private float m_horizontalMov;
+    private float m_verticalMov;
+    private Vector3 m_moveDirection;
     
     [Header("Drag")]
     private float m_groundedDrag = 6.0f;
     private float m_airDrag = 2.0f;
 
     [Header("Jumping")] 
-    [SerializeField] private float m_jumpForce = 5.0f; 
-
-    private float m_horizontalMov;
-    private float m_verticalMov;
+    [SerializeField] private float m_jumpForce = 5.0f;
+    
+    [Header("Ground Detection")]
     private bool m_isGrounded;
-    private Vector3 m_moveDirection;
+    private float m_groundDistance = 0.4f;
+    [SerializeField] private LayerMask m_groundMask;
+    
+    //Rigidbody
     private Rigidbody m_playerRb;
 
     private void Start()
@@ -33,7 +40,7 @@ public class CharaMovement : MonoBehaviour
 
     private void Update()
     {
-        m_isGrounded = Physics.Raycast(transform.position, Vector3.down, m_playerHeight / 2 + 0.1f);
+        m_isGrounded = Physics.CheckSphere(transform.position - new Vector3(0, 1, 0), m_groundDistance, m_groundMask);
         
         PlayerInputs();
         ControlDrag();
@@ -54,7 +61,7 @@ public class CharaMovement : MonoBehaviour
         m_horizontalMov = Input.GetAxisRaw("Horizontal");
         m_verticalMov = Input.GetAxisRaw("Vertical");
 
-        m_moveDirection = transform.forward * m_verticalMov + transform.right * m_horizontalMov;
+        m_moveDirection = m_orientation.forward * m_verticalMov + m_orientation.right * m_horizontalMov;
     }
 
     private void PlayerMove()
