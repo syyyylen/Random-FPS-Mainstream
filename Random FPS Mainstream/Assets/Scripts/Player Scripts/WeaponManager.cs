ddc1565fe2edc7f75b5b615747abb15113a4302e
Jules Vannier <julesvannier44@gmail.com>
2021-06-23 14:33:16 +0200

    Hitmarker / Reloading / Ammo In Mag

--- Random FPS Mainstream/Assets/Scripts/Player Scripts/WeaponManager.cs
+++ Random FPS Mainstream/Assets/Scripts/Player Scripts/WeaponManager.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Security.Cryptography;
-using UnityEditorInternal.Profiling.Memory.Experimental;
+﻿using System.Collections;
 using UnityEngine;
+using TMPro;
 
 public class WeaponManager : MonoBehaviour
 {
@@ -15,11 +13,27 @@ public class WeaponManager : MonoBehaviour
     [SerializeField] private Camera m_playerCamera;
     [SerializeField] private Transform m_orientation;
     
-    private float m_range = 500.0f;
+    [Header("HitMarker")]
+    [SerializeField] private GameObject m_hitMarker;
+    private Coroutine m_hitMarkerRoutine;
+    private float m_hitMarkerDuration = 0.15f;
+    
+    private float m_range = 100.0f;
+    private float m_fireTimer;
+    private int m_bulletsInMag;
+    [SerializeField] private TextMeshProUGUI m_bulletsInMagText;
 
+    //About Reload 
+    private KeyCode m_reloadKey = KeyCode.R;
+    private bool m_isReloading = false;
+    private Coroutine m_reloadRoutine;
+    [SerializeField] private GameObject m_reloadText;
+    
     private void Start()
     {
         SetEquipedWeapon(m_weapon);
+        m_bulletsInMag = m_weapon.m_weaponMagazine;
+        UpdateUI();
     }
 
     private void SetEquipedWeapon(WeaponSO p_newWeapon)
@@ -32,10 +46,18 @@ public class WeaponManager : MonoBehaviour
     {
         MoveWeapon();
 
-        if (Input.GetButton("Fire1"))
+        if (Input.GetKeyDown(m_reloadKey))
+        {
+            Reload();
+        }
+
+        if (m_isReloading != true &  m_bulletsInMag > 0 & Input.GetButton("Fire1"))
         {
             Fire();
         }
+
+        if (m_fireTimer < m_weapon.m_weaponRateOfFire)
+            m_fireTimer += Time.deltaTime;
     }
 
     private void MoveWeapon()
@@ -46,10 +68,56 @@ public class WeaponManager : MonoBehaviour
 
     private void Fire()
     {
+        if (m_fireTimer < m_weapon.m_weaponRateOfFire) return;
+
+        m_bulletsInMag -= 1;
+        UpdateUI();
+        
         RaycastHit hit;
         if (Physics.Raycast(m_playerCamera.transform.position, m_playerCamera.transform.forward, out hit, m_range))
         {
             Debug.Log(hit.transform.name);
+            HitMarker();
         }
+
+        m_fireTimer = 0.0f;
+    }
+
+    private void HitMarker()
+    {
+        if(m_hitMarkerRoutine != null)
+            StopCoroutine(m_hitMarkerRoutine);
+        m_hitMarkerRoutine = StartCoroutine(HitMarkerRoutine());
+        m_hitMarker.SetActive(true);
+    }
+
+    private IEnumerator HitMarkerRoutine()
+    {
+        yield return new WaitForSeconds(m_hitMarkerDuration);
+        m_hitMarker.SetActive(false);
+        
+    }
+
+    private void Reload()
+    {
+        m_isReloading = true;
+        m_reloadText.SetActive(true);
+        if(m_reloadRoutine != null)
+            StopCoroutine(m_reloadRoutine);
+        m_reloadRoutine = StartCoroutine(ReloadRoutine());
+    }
+
+    private IEnumerator ReloadRoutine()
+    {
+        yield return new WaitForSeconds(m_weapon.m_weaponReloadTime);
+        m_bulletsInMag = m_weapon.m_weaponMagazine;
+        UpdateUI();
+        m_isReloading = false;
+        m_reloadText.SetActive(false);
+    }
+
+    private void UpdateUI()
+    {
+        m_bulletsInMagText.text = m_bulletsInMag + "/" + m_weapon.m_weaponMagazine;
     }
 }
